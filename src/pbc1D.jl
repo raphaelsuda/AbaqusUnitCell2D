6964028d19d3d2ec7472125f1ce7fcb228c2308e
raphaelsuda <48560376+raphaelsuda@users.noreply.github.com>
2021-09-13 11:00:01 +0200

    Insert correction of coordinates to equations

--- src/pbc1D.jl
+++ src/pbc1D.jl
@@ -10,6 +10,8 @@ function pbc1D!(abq::AbqModel)
 	c = abq.csys
 	# abbreviation for unit cell dimensions
 	l = abq.dim
+	# abbreviation for minimum coordinates of unit cell
+	min_c = abq.minC
 	# initiate counter for equation numbering
 	i=1
 	################
@@ -26,7 +28,13 @@ function pbc1D!(abq::AbqModel)
 			i+=1
 			push!(abq.eqns,Equation(i,["E-$(n)","W-$(n)","SE","SW","NE","NW"],
 									  [c[a],c[a],c[a],c[a],c[a],c[a]],
-									  [1.0,-1.0,-1-x[c[2]]/l[c[2]],1.0+x[c[2]]/l[c[2]],-x[c[2]]/l[c[2]],x[c[2]]/l[c[2]]]))
+									  [+1.0,
+									   -1.0,
+									   -1.0 + (x[c[2]] - min_c[c[2]])/l[c[2]],
+									   +1.0 - (x[c[2]] - min_c[c[2]])/l[c[2]],
+									   -(x[c[2]] - min_c[c[2]])/l[c[2]],
+									   +(x[c[2]] - min_c[c[2]])/l[c[2]]
+									  ]))
 		end
 	end
 	println("$(i) equations written to AbqModel.")
